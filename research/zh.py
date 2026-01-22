13edeceaf3060327d0c142d5d91a08e1d999d453
lyy204 <13681662448@163.com>
2026-01-22 16:54:14 +0800

    add: kline_plot

--- research/zh.py
+++ research/zh.py
@@ -1,5 +1,7 @@
+import datetime
 import math
 
+import altair as alt
 import polars as pl
 
 OPEN = pl.col("open")
@@ -102,3 +104,64 @@ def backtest(
         ).show()
 
     return ret
+
+
+def kline_plot(
+    data: pl.DataFrame | pl.LazyFrame,
+    condition: pl.Expr,
+    pre: int = 5,
+    post: int = 5,
+):
+    data = data.lazy().sort([SYMBOL, OPEN_TIME])
+
+    offsets = range(-pre, post + 1)
+    cols = [
+        ((pl.col(f"{t}").shift(-i)) / OPEN.shift(-1)).over(SYMBOL).alias(f"{t}_{i}")
+        for i in offsets
+        for t in ["open", "high", "low", "close"]
+    ]
+    data = (
+        data.filter(
+            pl.any_horizontal([condition.shift(i).over(SYMBOL) for i in offsets])
+        )
+        .with_columns(cols)
+        .filter(condition)
+        .group_by(OPEN_TIME)
+        .agg(
+            [
+                pl.col(f"{t}_{i}").mean()
+                for i in offsets
+                for t in ["open", "high", "low", "close"]
+            ]
+        )
+        .select(pl.all().mean())
+        .collect(engine="streaming")
+    )
+
+    base_date = datetime.datetime(2020, 1, 1)
+    data = pl.DataFrame(
+        {
+            "date": [(base_date + datetime.timedelta(days=i)) for i in offsets],
+            "open": [data[f"open_{i}"][0] for i in offsets],
+            "high": [data[f"high_{i}"][0] for i in offsets],
+            "low": [data[f"low_{i}"][0] for i in offsets],
+            "close": [data[f"close_{i}"][0] for i in offsets],
+        }
+    )
+    open_close_color = (
+        alt.when("datum.open <= datum.close")
+        .then(alt.value("#06982d"))
+        .otherwise(alt.value("#ae1325"))
+    )
+    base = alt.Chart(data).encode(
+        alt.X("date:T"),
+        color=open_close_color,
+        tooltip=["date:T", "open:Q", "high:Q", "low:Q", "close:Q"],
+    )
+    rule = base.mark_rule().encode(
+        alt.Y("low:Q").title("Price").scale(zero=False), alt.Y2("high:Q")
+    )
+    bar = base.mark_bar().encode(alt.Y("open:Q"), alt.Y2("close:Q"))
+    (rule + bar).interactive().show()
+
+    return None
